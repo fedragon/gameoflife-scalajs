5f9161edad72c8d912bf5fad792479ad472fefd0
Federico Ragona <federico.ragona@gmail.com>
2015-06-09 10:32:23 +0200

    Generate arbitrary sized boards

--- src/main/scala/example/ScalaJSExample.scala
+++ src/main/scala/example/ScalaJSExample.scala
@@ -2,12 +2,14 @@ package example
 import scala.scalajs.js.annotation.JSExport
 import org.scalajs.dom
 import org.scalajs.dom.html
-import scala.util.Random
 
 @JSExport
 object GameOfLife {
   val Alive = true
   val Dead = false
+
+  val BoardSize = 10
+  val CellSize = 100
 
   case class Cell(x: Int, y: Int, alive: Boolean) {
     def willSurvive(neighbours: Seq[Cell]) = {
@@ -32,7 +34,7 @@ object GameOfLife {
 
       def move(dx: Int, dy: Int) = (x + dx, y + dy)
 
-      def square = {
+      def possibleNeighbours = {
         move(-1, -1) :: move(-1, 0) :: move(-1, 1) ::
         move(0, -1) :: move(0, 1) ::
         move(1, -1) :: move(1, 0) :: move(1, 1) ::
@@ -40,96 +42,59 @@ object GameOfLife {
       }
   }
 
-  def isValidMove(xy: (Int, Int)) = {
+  def exists(xy: (Int, Int)) = {
     val (x, y) = xy
-    x >= 0 && x < 3 && y >=0 && y < 3
+    x >= 0 && x < BoardSize && y >=0 && y < BoardSize
   }
 
   def neighboursOf(cell: Cell, cells: Seq[Cell]) = {
-    val indexes = cell.square.filter(isValidMove)
+    val indexes = cell.possibleNeighbours.filter(exists)
     cells.filter {
       case Cell(x, y, _) => indexes.contains((x, y))
     }
   }
 
-  def nextGeneration(cells: Seq[Cell]) =
+  def tick(cells: Seq[Cell]) =
     cells.zipWithIndex.map {
       case (cell, index) =>
         val neighbours = neighboursOf(cell, cells)
-        val alien = cell.evolve(neighbours)
-        println(s"cell: $cell, neighbours: $neighbours => $alien")
-        alien
+        cell.evolve(neighbours)
     }
 
-  //def overcrowded = {
-    //val cells = Seq(Cell(Alive), Cell(Alive), Cell(Alive),
-                    //Cell(Alive), Cell(Alive), Cell(Alive),
-                    //Cell(Alive), Cell(Alive), Cell(Alive))
-
-    //println("overcrowded: " + nextGeneration(cells))
-  //}
-
-  //def underpopulated = {
-    //val cells = Seq(Cell(Dead),  Cell(Dead),  Cell(Dead),
-                    //Cell(Alive), Cell(Alive), Cell(Dead),
-                    //Cell(Dead),  Cell(Dead),  Cell(Dead))
-
-    //println("underpopulated: " + nextGeneration(cells))
-  //}
-
-  //def survive = {
-    //val cells = Seq(Cell(Dead),  Cell(Dead),  Cell(Dead),
-                    //Cell(Alive), Cell(Alive), Cell(Alive),
-                    //Cell(Dead),  Cell(Dead),  Cell(Dead))
-
-    //println("survive: " + nextGeneration(cells))
-  //}
-
-  //def reproduce = {
-    //val cells = Seq(Cell(Dead),  Cell(Alive),  Cell(Dead),
-                    //Cell(Alive), Cell(Alive), Cell(Alive),
-                    //Cell(Dead),  Cell(Alive),  Cell(Dead))
-
-    //println("reproduce: " + nextGeneration(cells))
-  //}
-
-  //@JSExport
-  //def main(): Unit = {
-    //overcrowded
-    //underpopulated
-    //survive
-    //reproduce
-  //}
+  def generateBoard(aliveCells: Seq[(Int, Int)]): Seq[Cell] = {
+    for {
+      i <- 0 until BoardSize
+      j <- 0 until BoardSize
+    } yield {
+      Cell(i, j, aliveCells.contains((i, j)))
+    }
+  }
 
   @JSExport
   def main(canvas: html.Canvas): Unit = {
     val ctx = canvas.getContext("2d")
                     .asInstanceOf[dom.CanvasRenderingContext2D]
 
-    var cells = Seq(Cell(0, 0, Dead),  Cell(0, 1, Alive), Cell(0, 2, Dead),
-                    Cell(1, 0, Alive), Cell(1, 1, Alive), Cell(1, 2, Dead),
-                    Cell(2, 0, Dead),  Cell(2, 1, Dead),  Cell(2, 2, Alive))
+    canvas.width = BoardSize * CellSize
+    canvas.height = BoardSize * CellSize
+
+    var seed = generateBoard(Seq((1, 0), (1, 1), (1, 2), (0, 1), (2, 1)))
+    println(s"seed: $seed")
 
     ctx.fillStyle = "black"
-    ctx.fillRect(0, 0, 300, 300)
+    ctx.fillRect(0, 0, CellSize * BoardSize, CellSize * BoardSize)
 
     def run = {
-      val size = 85
-      cells.zipWithIndex.map { case (cell, index) =>
-        val y = cell.y * size
-        val x = cell.x * size
+      seed.zipWithIndex.map { case (cell, index) =>
+        val x = cell.x * CellSize
+        val y = cell.y * CellSize
         val style = if (cell.alive) "green" else "red"
-        println(s"$index => ($x, $y) => $style")
-
-        //ctx.fillStyle = "black"
-        //ctx.rect(x, y, size, size)
-        //ctx.stroke
 
         ctx.fillStyle = style
-        ctx.fillRect(x, y, size, size)
+        ctx.fillRect(x, y, CellSize, CellSize)
       }
 
-      cells = nextGeneration(cells)
+      seed = tick(seed)
     }
 
     dom.setInterval(() => run, 1000)
